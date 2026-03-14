2e2b0aaa6d10b53355f68935e3f59e86e4ad09fa
d1ana08 <jolchubaevadiana@gmail.com>
2026-03-14 12:16:05 +0600

    admin

--- course_app/admin.py
+++ course_app/admin.py
@@ -1,73 +1,78 @@
 from django.contrib import admin
-from .models import *
+from .models import (
+    UserProfile, Group, Student, Material,
+    Homework, HomeworkAnswer, CourseTest,
+    TestQuestion, TestAnswer, StudentTestResult,
+    Rating, Review
+)
+
 
 class GroupInline(admin.TabularInline):
     model = Group
     extra = 1
 
-class UserProfileAdmin(admin.ModelAdmin):
-    inlines = [GroupInline]
+class StudentInline(admin.TabularInline):
+    model = Student
+    extra = 1
 
-class StudentAdmin(admin.ModelAdmin):
-    inlines = [GroupInline]
-
-class MaterialAdmin(admin.ModelAdmin):
-    inlines = [GroupInline]
-
-class HomeworkAdmin(admin.ModelAdmin):
-    inlines = [GroupInline]
-
-class CourseTestAdmin(admin.ModelAdmin):
-    inlines = [GroupInline]
+class MaterialInline(admin.TabularInline):
+    model = Material
+    extra = 1
 
 class HomeworkInline(admin.TabularInline):
     model = Homework
     extra = 1
 
-class HomeworkAnswerAdmin(admin.ModelAdmin):
-    inlines = [HomeworkInline]
+class HomeworkAnswerInline(admin.TabularInline):
+    model = HomeworkAnswer
+    extra = 1
 
 class TestQuestionInline(admin.TabularInline):
     model = TestQuestion
     extra = 1
 
-class CourseTestAdmin(admin.ModelAdmin):
-    inlines = [TestQuestionInline]
-
 class TestAnswerInline(admin.TabularInline):
     model = TestAnswer
     extra = 1
-
-class TestQuestionAdmin(admin.ModelAdmin):
-    inlines = [TestAnswerInline]
 
 class StudentTestResultInline(admin.TabularInline):
     model = StudentTestResult
     extra = 1
 
-class CourseTestAdmin(admin.ModelAdmin):
-    inlines = [StudentTestResultInline]
-
 class RatingInline(admin.TabularInline):
     model = Rating
     extra = 1
-
-class GroupAdmin(admin.ModelAdmin):
-    inlines = [RatingInline]
 
 class ReviewInline(admin.TabularInline):
     model = Review
     extra = 1
 
+
+
 class UserProfileAdmin(admin.ModelAdmin):
-    inlines = [ReviewInline]
+    inlines = [GroupInline]
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [StudentInline, MaterialInline, HomeworkInline, RatingInline]
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [HomeworkAnswerInline]
+
+class HomeworkAdmin(admin.ModelAdmin):
+    inlines = [HomeworkAnswerInline, ReviewInline]
+
+class CourseTestAdmin(admin.ModelAdmin):
+    inlines = [TestQuestionInline, StudentTestResultInline]
+
+class TestQuestionAdmin(admin.ModelAdmin):
+    inlines = [TestAnswerInline]
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Group)
-admin.site.register(Student)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Material)
-admin.site.register(Homework)
+admin.site.register(Homework, HomeworkAdmin)
 admin.site.register(HomeworkAnswer)
 admin.site.register(CourseTest, CourseTestAdmin)
 admin.site.register(TestQuestion, TestQuestionAdmin)
